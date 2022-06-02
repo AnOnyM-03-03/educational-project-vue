7981601e2b85310ebfe884120ee711ec6d830a64
Tolstov <tolstov.dmitrij42@gmail.com>
2022-06-02 21:58:00 +0400

    main page car

---
+++ src/pages/Cars.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Cars page</h1>
+    <ul>
+        <router-link tag="li" v-for="car in 5" :key="car" :to="'/car/' + car"  >
+        <a>car {{ car }}</a>
+        </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+}
+</script>
+
+<style>
+</style>
