440554ba81bb4f0cfc4e876c2d1a059ff5b11c03
Tolstov <tolstov.dmitrij42@gmail.com>
2022-06-02 21:52:48 +0400

    added markup to homepage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
-  <div >
-    
+  <div class="container">
+    <nav class="navbar navbar-expand-lg bg-light navbar-light">
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <router-link tag="li" class="nav-item" exact to="/" active-class="active" >
+            <a class="nav-link">Home</a>
+        </router-link>
+        <router-link tag="li" class="nav-item" to="/cars" active-class="active" >
+            <a class="nav-link">Cars</a>
+        </router-link>
+        <router-link tag="li" class="nav-item" to="/car/3" active-class="active" >
+            <a class="nav-link">car 3</a>
+        </router-link>
+        <router-link tag="li" class="nav-item" to="/car/4" active-class="active" >
+            <a class="nav-link">car 4</a>
+        </router-link>
+      </ul>
+    </div>
+</nav>
+      <router-view></router-view>
   </div>
 </template>
 
